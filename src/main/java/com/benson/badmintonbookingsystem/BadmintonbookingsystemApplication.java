e0e40857d841169090229ddcd6809a55825c5404
Benson Yang <f932037@gmail.com>
2023-06-27 00:12:18 +0800

    init project

---
+++ src/main/java/com/benson/badmintonbookingsystem/BadmintonbookingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.benson.badmintonbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadmintonbookingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadmintonbookingsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benson/badmintonbookingsystem/BadmintonbookingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benson.badmintonbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadmintonbookingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
